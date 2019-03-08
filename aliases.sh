76384c90dc51103fcd96e64a73a7614bc3197446
Matthew Miller <matthew@millerti.me>
2019-03-07 20:32:44 -0800

    Add an alias to emulate nvmrc support with n

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,5 @@ alias ping="ping -c 4"
 alias flushdns="sudo killall -HUP mDNSResponder"
 alias findtext="grep -rnw ./ -e"
 alias dc="docker-compose"
+# Enable "nvm use"-esque functionality with n
+alias nu='n $(cat .nvmrc)'
