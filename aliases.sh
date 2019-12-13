bd348770a1453641b87a618a7f0b07866ece9069
Matthew Miller <matthew@millerti.me>
2019-12-13 12:26:08 -0800

    Remove `nu` alias

--- aliases.sh
+++ aliases.sh
@@ -2,5 +2,3 @@ alias ping="ping -c 4"
 alias flushdns="sudo killall -HUP mDNSResponder"
 alias findtext="grep -rnw ./ -e"
 alias dc="docker-compose"
-# Enable "nvm use"-esque functionality with n
-alias nu='n $(cat .nvmrc)'
