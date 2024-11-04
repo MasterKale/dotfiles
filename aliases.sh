5bb6b8fe72fc600160bda0974e961fb2e4620b5e
Matthew Miller <matthew@millerti.me>
2024-11-03 21:14:20 -0800

    Add pyserv alias

--- aliases.sh
+++ aliases.sh
@@ -4,6 +4,7 @@ alias findtext="grep -rnw ./ -e"
 alias dc="docker compose"
 alias pva="source venv/bin/activate"
 alias tower="gittower ."
+alias pyserv="python3 -m http.server -b localhost"
 
 makerandpass() {
   # A simple command to generate a simple, random password. Think "Webex video link password"
